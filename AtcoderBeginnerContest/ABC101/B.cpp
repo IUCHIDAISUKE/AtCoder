a17afd0f09eb9df8035f67bb36858a52be38baf5
Iuchi Daisuke <33daisuke07@gmail.com>
2020-09-08 10:02:47 +0900

    ABC096~ABC104

---
+++ AtcoderBeginnerContest/ABC101/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep2(i, a, b) for (int i = (a); i < (b); ++i)
+#define all(a) (a).begin(), (a).end()
+
+int sum_digit(int n)
+{
+    int res = 0;
+    while (n)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << ((n % sum_digit(n) == 0) ? "Yes" : "No") << '\n';
+    return (0);
+}
