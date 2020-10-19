7da379bef9ff0ccdb8383751721b7c7ed95a4ac0
Iuchi Daisuke <33daisuke07@gmail.com>
2020-10-19 23:31:36 +0900

    HHKB,M-SOLUTION,エイシング

---
+++ M-SOLUTIONS プロコンオープン 2020/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep2(i, a, b) for (int i = (a); i < (b); ++i)
+#define all(a) (a).begin(), (a).end()
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    cout << 10 - (x / 200) << '\n';
+    return 0;
+}
