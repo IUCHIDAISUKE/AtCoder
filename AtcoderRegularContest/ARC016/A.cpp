557fdc134f314f9c3d9afa8fbc9085715efddff9
DAISUKE <33daisuke07@gmail.com>
2021-01-01 21:38:26 +0900

    ARC009,015~017

---
+++ AtcoderRegularContest/ARC016/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep2(i, a, b) for (int i = (a); i < (b); ++i)
+#define all(a) (a).begin(), (a).end()
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+const ll INF = 1LL << 60;
+const int MOD = (int)1e9 + 7;
+const double PI = acos(-1);
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    if (m - 1 == 0)
+    {
+        cout << m + 1 << '\n';
+    }
+    else
+    {
+        cout << m - 1 << '\n';
+    }
+    return 0;
+}
